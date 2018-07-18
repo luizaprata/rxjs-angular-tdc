111b2a64caf413981d8daae8bc2ba011c1d51965
Luiza Prata Soldi Passos <luiza.psp@gmail.com>
2018-07-18 02:00:46 -0300

    ajustes finalizacao

--- src/app/model/SearchItem.ts
+++ src/app/model/SearchItem.ts
@@ -1,9 +1,14 @@
 export default class SearchItem {
-  constructor(
-    public track: string,
-    public artist: string,
-    public link: string,
-    public thumbnail: string,
-    public artistId: string
-  ) {}
+  public track: string;
+  public artist: string;
+  public link: string;
+  public thumbnail: string;
+  public artistId: string;
+  constructor(item) {
+    this.track = item.trackName;
+    this.artist = item.artistName;
+    this.link = item.trackViewUrl;
+    this.thumbnail = item.artworkUrl30;
+    this.artistId = item.artistId;
+  }
 }
--- src/app/service/search.service.ts
+++ src/app/service/search.service.ts
@@ -3,61 +3,53 @@ import { Http } from '@angular/http';
 import { map, tap, retryWhen, scan, takeWhile, delay } from 'rxjs/operators';
 import { Observable } from 'rxjs/internal/Observable';
 import SearchItem from '../model/SearchItem';
-import { ReplaySubject, BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, ReplaySubject } from 'rxjs';
 
 @Injectable()
 export class SearchService {
-  apiRoot: string = 'https://itunes.apple.com/search';
+  api: string = 'https://itunes.apple.com/search';
   results: SearchItem[];
-  logTerm = new ReplaySubject<string>(20);
-  lastTerm = new BehaviorSubject<string>('');
-  logCount: number = 0;
+  
+  
 
   constructor(private http: Http) {
     this.results = [];
+  }
+  
+  logCount: number = 0;
+
+  logTerm = new ReplaySubject<string>(5);
+  lastTerm = new BehaviorSubject<string>('');
+  
+  addLog(msg) {
+    let count = this.logCount++;
+    this.logTerm.next(`[${count}] ${msg}`);
+    this.lastTerm.next(`[${count}] ${msg}`);
   }
 
   retryStrategy(term) {
     let _self = this;
     return function(error) {
       return error
-        .pipe(
-          scan((acc, value) => {
-            _self.addLog(`${term} : falhou tentativa ${acc + 1} de 3`);
-            return acc + 1;
-          }, 0)
-        )
+        .pipe(scan((acc, value) => {
+          _self.addLog(`${term} : tentativa ${acc}`)
+          return acc + 1;
+        }, 0))
         .pipe(takeWhile((acc) => acc < 3))
         .pipe(delay(1500));
     };
   }
 
-  addLog(msg) {
-    this.logCount++;
-    msg = `[${this.logCount}] ${msg}`
-    this.logTerm.next(msg);
-    this.lastTerm.next(msg);
-  }
-
   search(term: string): Observable<SearchItem[]> {
-    const apiURL = `${this.apiRoot}?term=${term}&media=music&limit=20`;
-    this.addLog(`${term} : start request`);
+    const url = `${this.api}?term=${term}&media=music`;
+    this.addLog(`${term} : inicia request`)
     return this.http
-      .get(apiURL)
-      .pipe(tap((_) => this.addLog(`${term} : ok`)))
-      .pipe(
-        map((res) =>
-          res.json().results.map((item) => {
-            return new SearchItem(
-              item.trackName,
-              item.artistName,
-              item.trackViewUrl,
-              item.artworkUrl30,
-              item.artistId
-            );
-          })
-        )
-      )
+      .get(url)
+      .pipe(tap((_)=> this.addLog(`${term} : sucesso`)))
+      .pipe(map((res) => 
+        res.json().results.map((item) => 
+          new SearchItem(item))
+      ))
       .pipe(retryWhen(this.retryStrategy(term)));
   }
 }
